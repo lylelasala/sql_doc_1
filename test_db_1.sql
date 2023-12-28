bf188942c77960470ee6377ceabc0f872b65450d
lylelasala <102710270+lylelasala@users.noreply.github.com>
2023-12-27 18:17:45 -0800

    Add files via upload

--- test_db_1.sql
+++ test_db_1.sql
@@ -145,3 +145,63 @@ from customer
 where email like 'j__.w%'
 group by email 
 having COUNT(customer_id) > 0;
+
+--Week 5 - Monday Questions
+--
+--1. How many actors are there with the last name ‘Wahlberg’?
+select count(*)
+from actor 
+where last_name = 'Wahlberg';
+
+--2. edit How many payments were made between $3.99 and $5.99?
+select count(*)
+from payment 
+where amount between 3.99 and 5.99;
+
+-- feel like this code should work?
+--3. What film does the store have the most of? (search in inventory)
+select film_id, count(*)
+from inventory 
+group by film_id
+order by count(*) desc; 
+
+-- Got 193 with 8 :/
+
+--4. How many customers have the last name ‘William’?
+select count(*)
+from customers 
+where last_name = 'William';
+
+--5. What store employee (get the id) sold the most rentals?
+select staff_id, count(rental_id)
+from rental
+group by staff_id
+order by count(rental_id) desc;
+
+--6. edit How many different district names are there?
+select district_name, count(district_name)
+from district
+group by district_name; 
+
+--7. What film has the most actors in it? (use film_actor table and get film_id)
+select film_id, count(film_id) as num_actors
+from film_actor 
+group by film_id 
+order by count(film_id) desc;
+
+--8. From store_id 1, how many customers have a last name ending with ‘es’? (use customer table)
+select count(*)
+from customer
+where store_id = 1 and last_name like '%es';
+
+--9. How many payment amounts (4.99, 5.99, etc.) had a number of rentals above 250 for customers
+--with ids between 380 and 430? (use group by and having > 250)
+select amount, count(rental_id) as rentals
+from payment 
+where customer_id between 380 and 430
+group by amount having count(rental_id) > 250;
+
+--10. Within the film table, how many ratings categories are there? And what rating has the most movies total?
+select rating, count(*)
+from film 
+group by rating;
